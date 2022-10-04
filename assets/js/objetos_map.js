562c9f17ffd2df138ac1b168d08ddad200eb108b
elis mendonca <elis_pm@hotmail.com>
2022-10-04 16:55:55 -0300

    Trabalhando com objetos parte 2, comitando o html e css

---
+++ assets/js/objetos_map.js
@@ -0,0 +1,13 @@
+let btnCalcular = document.getElementById("calcular");
+let btnLimpar = document.getElementById("limpar");
+let numero = document.getElementById("numero");
+let arrValores = [];
+let resultadoDiv = document.getElementById("resultado");
+
+let valorDigitado = btnCalcular.map(function (numeroDigitado) {
+  return numeroDigitado.arrValores;
+  console.log(valorDigitado(numero));
+});
+
+btnCalcular.addEventListener("click", arrValores);
+btnLimpar.addEventListener("click");
